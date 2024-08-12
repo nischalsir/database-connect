cb7a714c451ac81d8fa6ba82a0e47cc131f9e7dd
nischalsir <pandenishchal@gmail.com>
2024-08-12 21:28:58 +0545

    nisch

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Login | Class</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
